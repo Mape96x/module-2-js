1a8b8f6f84e3b7b19455c5c9b2c16b4c34b41f14
Marek Pentzek <marekpj96@gmail.com>
2023-11-07 12:24:24 +0100

    Solution Filter-API-Data

--- recap-tasks/coding-foundation-katas-main/filter-api-data/filter-api-data.js
+++ recap-tasks/coding-foundation-katas-main/filter-api-data/filter-api-data.js
@@ -1,3 +1,21 @@
 function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+  let nArr = [];
+
+  for (let i = 0; i < apiData.length; i++) {
+    let obj = apiData[i];
+    let hasAllKeys = true;
+
+    for (let j = 0; j < mandatoryKeys.length; j++) {
+      let key = mandatoryKeys[j];
+      if (!obj.hasOwnProperty(key)) {
+        hasAllKeys = false;
+        break;
+      }
+    }
+
+    if (hasAllKeys) {
+      nArr.push(obj);
+    }
+  }
+  return nArr;
 }
