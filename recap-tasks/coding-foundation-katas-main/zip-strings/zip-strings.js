b6d7f098f123622d9daeb66380e1f521d7376dbc
Marek Pentzek <marekpj96@gmail.com>
2023-11-07 10:16:36 +0100

    Zip Strings Solution

---
+++ recap-tasks/coding-foundation-katas-main/zip-strings/zip-strings.js
@@ -0,0 +1,18 @@
+function zipStrings(strA, strB) {
+  const arr1 = strA.split("");
+  const arr2 = strB.split("");
+  const newArr = [];
+
+  if (arr1 > arr2) {
+    for (let i = 0; i < strA.length; i++) {
+      newArr.push(arr1[i]);
+      newArr.push(arr2[i]);
+    }
+  } else if (arr1 < arr2) {
+    for (let i = 0; i < strB.length; i++) {
+      newArr.push(arr1[i]);
+      newArr.push(arr2[i]);
+    }
+  }
+  return newArr.join("");
+}
