a89c32ea51f6f78aff46f6c06b61e031f26b06f0
Marek Pentzek <marekpj96@gmail.com>
2023-11-08 12:03:39 +0100

    crippled solution

--- recap-tasks/coding-foundation-katas-main/filter-api-data/filter-api-data.js
+++ recap-tasks/coding-foundation-katas-main/filter-api-data/filter-api-data.js
@@ -1,21 +1,25 @@
 function filterApiData(apiData, mandatoryKeys) {
-  let nArr = [];
+  // let nArr = [];
 
-  for (let i = 0; i < apiData.length; i++) {
-    let obj = apiData[i];
-    let hasAllKeys = true;
+  // for (let i = 0; i < apiData.length; i++) {
+  //   let obj = apiData[i];
+  //   let hasAllKeys = true;
 
-    for (let j = 0; j < mandatoryKeys.length; j++) {
-      let key = mandatoryKeys[j];
-      if (!obj.hasOwnProperty(key)) {
-        hasAllKeys = false;
-        break;
-      }
-    }
+  //   for (let j = 0; j < mandatoryKeys.length; j++) {
+  //     let key = mandatoryKeys[j];
+  //     if (!obj.hasOwnProperty(key)) {
+  //       hasAllKeys = false;
+  //       break;
+  //     }
+  //   }
 
-    if (hasAllKeys) {
-      nArr.push(obj);
-    }
-  }
-  return nArr;
+  //   if (hasAllKeys) {
+  //     nArr.push(obj);
+  //   }
+  // }
+  // return nArr;
+
+  return apiData.filter((obj) => {
+    return mandatoryKeys.every((key) => obj.hasOwnProperty(key));
+  });
 }
